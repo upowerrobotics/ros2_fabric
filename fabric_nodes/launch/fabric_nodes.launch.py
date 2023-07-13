cbc898e01f5edb3a2aef0e68cbaec23a9ff0066b
I-Hsi Kao <i-hsi.kao@upower.com>
2023-07-13 11:38:23 -0700

    adjust with new doc

--- fabric_nodes/launch/fabric_nodes.launch.py
+++ fabric_nodes/launch/fabric_nodes.launch.py
@@ -39,38 +39,65 @@ def generate_launch_description():
 
     # Process publish topics
     for environment in config['environments']:
-        if environment['name'] == 'env1':
-            # Process nodes
-            for node_config in environment['nodes']:
-                node_name = node_config['name']
-                root_node = node_config['root_node']
-                terminal_node = node_config['terminal_node']
+        if environment['name'] != 'env1':
+            continue
+        
+        # Process nodes
+        for node_config in environment['nodes']:
+            node_name = node_config['name']
+            root_node = node_config['root_node']
+            terminal_node = node_config['terminal_node']
+            node_qty = node_config.get('qty', 1)
 
-                # Process publishers
-                publish_topics = {}
-                if 'publishers' in node_config:
-                    for publisher in node_config['publishers']:
-                        topic_name = publisher['name']
-                        publish_topic = {}
-                        if 'msg_size' in publisher:
-                            publish_topic['msg_size'] = publisher['msg_size']
-                        if 'bandwidth' in publisher:
-                            publish_topic['bandwidth'] = publisher['bandwidth']
-                        if 'msg_frequency' in publisher:
-                            publish_topic['msg_frequency'] = publisher['msg_frequency']
-                        if publish_topic:
-                            publish_topics[topic_name] = publish_topic
+            # Process publishers
+            publish_topics = {}
+            if 'publishers' in node_config:
+                for publisher in node_config['publishers']:
+                    topic_name = publisher['name']
+                    publish_topic = {}
+                    if 'msg_size' in publisher:
+                        publish_topic['msg_size'] = publisher['msg_size']
+                    if 'bandwidth' in publisher:
+                        publish_topic['bandwidth'] = publisher['bandwidth']
+                    if 'msg_frequency' in publisher:
+                        publish_topic['msg_frequency'] = publisher['msg_frequency']
+                    publisher_qty = publisher.get('qty', 1)
+                    if publisher_qty != 1:
+                        for num in range(1, publisher_qty + 1):
+                            publish_topics[topic_name + '_' + str(num)] = publish_topic
+                    else:
+                        publish_topics[topic_name] = publish_topic
 
-                # Process subscribers
-                subscribe_topics = {}
-                if 'subscribers' in node_config:
-                    for subscriber in node_config['subscribers']:
-                        topic_name = subscriber['name']
-                        subscribe_topic = {
-                            'node': subscriber['node']
-                        }
-                        subscribe_topics[topic_name] = subscribe_topic      
-
+            # Process subscribers
+            subscribe_topics = {}
+            if 'subscribers' in node_config:
+                for subscriber in node_config['subscribers']:
+                    topic_name = subscriber['name']
+                    subscribe_topic = {'node': subscriber['node']}
+                    subscriber_qty = subscriber.get('qty', 1)
+                    if subscriber_qty != 1:
+                        for num in range(1, subscriber_qty + 1):
+                            subscribe_topics[topic_name + '_' + str(num)] = subscribe_topic
+                    else:
+                        subscribe_topics[topic_name] = subscribe_topic
+            
+            if node_qty != 1:
+                for num in range(1, node_qty + 1):
+                    node = Node(
+                        package='fabric_nodes',
+                        executable='dummy_node_exe',
+                        name=node_name + '_' + str(num),
+                        namespace=node_name + '_' + str(num),
+                        parameters=[{
+                            'root_node': root_node,
+                            'terminal_node': terminal_node,
+                            'publish_topics': publish_topics,
+                            'subscribe_topics': subscribe_topics,
+                        }],
+                        output='screen'
+                    )
+                    nodes.append(node)
+            else:
                 node = Node(
                     package='fabric_nodes',
                     executable='dummy_node_exe',

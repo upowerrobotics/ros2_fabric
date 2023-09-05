b60261d3b554694e99278410f1d5afa9dbe6e829
LawrenceXu13467 <LawrenceXu13467@users.noreply.github.com>
2023-09-05 21:09:43 +0000

    deploy: 4aea272e1e4d4d712ee29e54780be4fd51c0b155

--- search/classes_3.js
+++ search/classes_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['publishtopic_70',['PublishTopic',['../structfabric__nodes_1_1PublishTopic.html',1,'fabric_nodes']]]
+  ['plotcsv_67',['PlotCSV',['../classfabric__nodes_1_1plot__csv_1_1PlotCSV.html',1,'fabric_nodes::plot_csv']]],
+  ['publishtopic_68',['PublishTopic',['../structfabric__nodes_1_1PublishTopic.html',1,'fabric_nodes']]]
 ];
